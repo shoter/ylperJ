ebc1b079b4074e7fe0150604839a150992c4d61e
shoter <lubiebardzozelki@gmail.com>
2019-07-10 01:50:49 +0200

    Added CarErrors

---
+++ YlperJ-core/src/main/java/com/shoter/ylper/core/Cars/CarErrors.java
@@ -0,0 +1,7 @@
+package com.shoter.ylper.core.Cars;
+
+public class CarErrors {
+    public static final String carNotExist = "Car does not exist!";
+
+    public static final String cannotRemoveBecauseBookings = "You cannot remove a car that has bookings!";
+}
